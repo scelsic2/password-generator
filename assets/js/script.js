0326be79540d1c96e3122b9614fb065e6e6fc0c8
scelsic2 <scelsic2@gmail.com>
2023-03-11 16:58:34 -0500

    final product

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-// Starting third attempt with a study group and a tutor
-// Random Password Generator in JavaScript. (2020, June 19). YouTube. Retrieved March 09, 2023, from https://www.youtube.com/watch?v=v2jfGo7ztm8
+// Starting third attempt with a another YouTube tutorial, a study group, and a tutor
+// Used this code as a model.  This is where I learned about confirm, prompt, and parseInt:  Random Password Generator in JavaScript. (2020, June 19). YouTube. Retrieved March 09, 2023, from https://www.youtube.com/watch?v=v2jfGo7ztm8
 
 // Assignment Code
-// Options that password generator can pull from
-//If you don't declare passwordLength up here, you can't find the length of it in generate password
+// Here are the options the password generator can pull from
+//If you don't declare passwordLength up here, you can't find the length of it in generatePassword
 var passwordLength = 0;
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
@@ -11,39 +11,45 @@ var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 var special = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", "|", "?", "<", ">"]
 
 
-// What are the prompts for password criteria?
+// Here are the prompts for password criteria.
 // JavaScript parseInt() Method. (n.d.). W3Schools. Retrieved March 9, 2023, from https://www.w3schools.com/jsref/jsref_parseint.asp
-// Going to add these values into the selectionCriteriaArray
+// Going to add these values into the selectionCriteriaArray.  I declare selectionCriteriaArray as a var INSIDE OF the function so that I can return it, then when I return it I need to set the return value to a variable so that I can access it later.
+// Store the promptValueGiven so that I can check to see if it is an integer.  parseInt is a special function within Javascript so when I pass promptValueGiven into it, it can tell me whether or not promptValueGiven is an integer.
+// JavaScript Number isInteger() Method. (n.d.). W3Schools. Retrieved March 11, 2023, from https://www.w3schools.com/jsref/jsref_isinteger.asp
+// isNaN() - JavaScript | MDN. (n.d.). MDN Web Docs. Retrieved March 11, 2023, from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
 function passwordPrompt() {
   var selectionCriteriaArray = [];
   
-  var promptValueGiven = prompt("How many characters do you want your password to have?");
-  // Store the prompt value given so that we can check to see if it is an integer
-  passwordLength = parseInt(promptValueGiven);
-
-  if (confirm("Include lowercase letters?")) {
-    selectionCriteriaArray = selectionCriteriaArray.concat(lowercase);
-  }
-
-  if (confirm("Include uppercase letters?")) {
-    selectionCriteriaArray = selectionCriteriaArray.concat(uppercase);
-  }
-
-  if (confirm("Include numbers?")) {
-    selectionCriteriaArray = selectionCriteriaArray.concat(numbers);
-  }
-
-  if (confirm("Include special characters?")) {
-    selectionCriteriaArray = selectionCriteriaArray.concat(special);
-  }
-  return selectionCriteriaArray;
+  var promptValueGiven = prompt("How many characters do you want your password to have?  You can choose between 8 and 128 characters.");
+  passwordLength = parseInt(promptValueGiven)
+    if (isNaN(promptValueGiven) == true) {
+      alert("Your value entered is not a number.")
+    }
+    else if (promptValueGiven < 8 || promptValueGiven > 128) {
+      alert("Your value must be between 8 and 128 chatacters.")
+    }
+    else {
+      if (confirm("Include lowercase letters?")) {
+      selectionCriteriaArray = selectionCriteriaArray.concat(lowercase);
+      }
+      if (confirm("Include uppercase letters?")) {
+      selectionCriteriaArray = selectionCriteriaArray.concat(uppercase);
+      }
+      if (confirm("Include numbers?")) {
+      selectionCriteriaArray = selectionCriteriaArray.concat(numbers);
+      }
+      if (confirm("Include special characters?")) {
+      selectionCriteriaArray = selectionCriteriaArray.concat(special);
+      }
+    }
+    return selectionCriteriaArray;
 }
 
-// This will only pull from the items that got pulled into the array based on the user-selected criteria
+// This function will only pull from the items that got pulled into the array based on the user-selected criteria
 // If you don't declare var password in the fuction, then you can't return it at the end of the function
-// The random character type picks - number, lowercase, uppercase, special
-// Then the random character picks a random character from the selected array (number, lowercase, uppercase, special)
-// When I concatinated my arrays I turned them into an object which is why I can't use just one math.floor/math.random to loop through everything I want it to loop through, because the index of the individual characters will be [i][i], not just [i].
+// The randomCharacterType picks either number, lowercase, uppercase, or special
+// Then the randomCharacter picks a random character from the selected array (number, lowercase, uppercase, special)
+// **When I concatinated my arrays, I turned them into an object which is why I can't use just one math.floor/math.random to loop through everything I want it to loop through, because the index of the individual characters are now [i][i], not just [i].**
 function generatePassword(selectionCriteriaArray){
   var password = '';
   for (i = 0; i < passwordLength; i++) {
@@ -55,21 +61,27 @@ function generatePassword(selectionCriteriaArray){
 }
  
 // Write password to the #password input
-// Write password requires a function called generate password
+// writePassword requires a function called generatePassword
 // Then take the password text response and tag it into the password id in the html
-// password prompt doesn't need anything passed into it because the function didn't have any parameteres when it was created
-// If I want the generate password function to work with the result of my passsword promot, I have to store my password prompt result into a variable and then pass that variable inside of the function generate password.  Generate password had a parameter when we created it, so we need to pass it the value took from password prompt in order for generate password to have the right data to work with.
-// Once I get the element by ID that I want to change, then I want to use the value property to change what displays in that element.  I am displaying the generate password function that ran from the password prompt criteria that was selected.
+// passwordPrompt doesn't need anything passed into it because the function didn't have any parameteres when it was created
+// If I want the generatePassword function to work with the result of my passswordPrompt, I have to store my passwordPrompt result into a variable and then pass that variable inside of the function generatePassword.  Generate password had a parameter when I created it, so I need to pass it the value created in passwordPrompt in order for generatePassword to have the right data to work with.
+// Once I get the element by ID that I want to change, then I want to use the value property to change what displays in that element.  I am displaying the generatePassword function that ran from the passwordPrompt criteria that was selected by the user.
+// passwordPrompt will run even if the user inputs NaN or if they don't enter enough characters; I can't stop it from running, so I need to add a prompt here to tell the user that the application can't generate them a password with their input.
 function writePassword() {
   var returnSelectionCriteriaArray = passwordPrompt();
-  var password = generatePassword(returnSelectionCriteriaArray);
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  if (returnSelectionCriteriaArray.length > 0) {
+    var password = generatePassword(returnSelectionCriteriaArray);
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
+  else {
+    alert("Please try again.");
+  }
 }
 
 // Add event listener to generate button
-// When generate button is cliked, it will run the function write password
-// Once I get the button element by ID, I can add an event listener to it. Once I have done that, then I can say when the user clicks on it that it should run the write password function.
+// When generate button is cliked, it will run the function writePassword
+// Once I get the button element by ID, I can add an event listener to it. Once I have done that, when the user clicks on it, it should run the writePassword function.
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
